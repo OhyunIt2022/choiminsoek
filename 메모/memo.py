16e4af50cb98fe949cecdfd92cb5d8475fe2929a
MinSeok1120 <107673668+MinSeok1120@users.noreply.github.com>
2022-07-27 17:49:37 +0900

    메모장 만들기 읽기기능

---
+++ 메모/memo.py
@@ -0,0 +1,23 @@
+from fileinput import filename
+from lzma import FILTER_LZMA2
+from optparse import Option
+
+
+print("hello world")
+print("진행할 작업을 선택해주세요")
+print("1.메모작성 2. 메모읽기")
+
+option = input()
+
+print(option)
+
+if option == '1':
+    print("아직 구현안함")
+elif option == '2':
+    filename = 'memo2.txt'
+    f = open(filename)
+    memo = f.read()
+    print(memo)
+    f.close()
+else:
+    prinr("잘봇 입력하셨습니다")
